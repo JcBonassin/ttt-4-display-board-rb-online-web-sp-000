5ebbc99a1c6e975bb5ce1e82955191db9cae0a91
JC <bonassin@gmail.com>
2020-12-03 16:34:10 +0000

    Automatically backed up by Learn

--- lib/display_board_spec.rb
+++ lib/display_board_spec.rb
@@ -1,14 +1,1 @@
-def display_board
-   puts "   |   |   "
-   puts "-----------"
-   puts "   |   |   "
-   puts "-----------"
-   puts "   |   |   "
-  end
-def display_board
-     puts "   |   |   "
-     puts "-----------"
-     puts "   | #{board[4]}  |   "
-     puts "-----------"
-     puts "   |   |   "
-   end
+

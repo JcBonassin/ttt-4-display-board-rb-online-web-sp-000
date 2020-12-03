e631ac9fce57a92324cd133b9fb1f6bbdbf7d5dd
JC <bonassin@gmail.com>
2020-12-03 16:34:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,7 @@
 def display_board
-   puts "   |   |   "
-   puts "-----------"
-   puts "   |   |   "
-   puts "-----------"
-   puts "   |   |   "
-  end
-def display_board
-     puts "   |   |   "
-     puts "-----------"
-     puts "   | #{board[4]}  |   "
-     puts "-----------"
-     puts "   |   |   "
-   end
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
